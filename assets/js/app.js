3d78e90fb22281d5ee5eed8a75044b2f71f6d762
Russel <kotashov03@gmail.com>
2024-04-03 16:05:39 +0400

    update from WP

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,19 @@
-document.addEventListener('DOMContentLoaded', ()=>{
+document.addEventListener('DOMContentLoaded', () => {
+
     const heroSlider = new Swiper('.hero__slider', {
-        spaceBetween: 15,
+        // modules: [SwiperGL],
+        effect: 'fade',
+        // gl: {
+        //     shader: 'wind',
+        //   },
+        autoplay: {
+            delay: 3000,
+        },
+        // shuttersEffect: {
+        //     split: 4,
+        // },
+        speed: 900,
+        parallax: true,
     })
 
     const catSlider = new Swiper('.slider-catalog__slider', {
@@ -21,7 +34,8 @@ document.addEventListener('DOMContentLoaded', ()=>{
             1200: {
                 slidesPerView: 6,
             },
-        }
+        },
+
     })
 
     const partsSlider = new Swiper('.partners__slider', {
@@ -62,8 +76,8 @@ document.addEventListener('DOMContentLoaded', ()=>{
     const factoryBtn = document.querySelectorAll('.factory__btn');
 
     if (factoryBtn.length != 0) {
-        factoryBtn.forEach(item=>{
-            item.addEventListener('click', ()=>{
+        factoryBtn.forEach(item => {
+            item.addEventListener('click', () => {
                 const currentText = item.nextElementSibling;
                 if (item.classList.contains('active')) {
                     item.classList.remove('active')
@@ -85,4 +99,88 @@ document.addEventListener('DOMContentLoaded', ()=>{
             item.style.maxHeight = item.scrollHeight + 'px';
         }
     }
+
+    const mobileMenubtn = document.querySelector('.burger-btn');
+    const closeMenu = document.querySelector('.close-menu');
+    let mobileMenu;
+
+    if (closeMenu) {
+        closeMenu.addEventListener('click', CloseMobileMenu);
+    }
+
+    if (mobileMenubtn) {
+        mobileMenubtn.addEventListener('click', toggleMobileMenu);
+    }
+
+    function toggleMobileMenu() {
+        mobileMenu = document.querySelector('.mobile__menu');
+        if (mobileMenu.classList.contains('active')) {
+            CloseMobileMenu();
+            
+        } else {
+            OpenMobileMenu(mobileMenu);
+            document.addEventListener('click', clickOutsideHandler);
+            document.addEventListener('keydown', function(event) {
+                if (event.key === "Escape") {
+                    CloseMobileMenu();
+                }
+            });
+        }
+    }
+
+    function clickOutsideHandler(event) {
+        if (!mobileMenu.contains(event.target) && event.target !== mobileMenubtn) {
+            CloseMobileMenu();
+            document.removeEventListener('click', clickOutsideHandler);
+        }
+    }
+
+    function CloseMobileMenu() {
+        mobileMenu.classList.remove('active');
+        BlackModal('off');
+        document.body.style.overflow = 'auto';
+    }
+
+    function OpenMobileMenu(menu) {
+        menu.classList.add('active');
+        BlackModal('on');
+        document.body.style.overflow = 'hidden';
+    }
+
+    function BlackModal(target) {
+        const blackModal = document.querySelector('.black_modal');
+        if (target == 'on') {
+            blackModal.classList.add('active');
+        } else {
+            blackModal.classList.remove('active');
+        }
+        
+    }
+
+
+    const categoryBtn = document.querySelector('.btn-category-show');
+
+    if (categoryBtn) {
+        const categoryListWrap = document.querySelector('.catalog__category-lists');
+        categoryBtn.addEventListener('click', (e)=>{
+            const target = e.target;
+            if (target.classList.contains('active')) {
+                target.classList.remove('active');
+                target.innerHTML = 'Показать категории';
+                CloseCategoryList();
+            } else {
+                target.classList.add('active');
+                OpenCategoryList();
+                target.innerHTML = 'Скрыть категории';
+            }
+        })
+        
+        function OpenCategoryList() {
+            categoryListWrap.classList.add('active');
+        }
+        function CloseCategoryList() {
+            categoryListWrap.classList.remove('active');
+        }
+    }
+
 })
